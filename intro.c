277fbd513778be1189561199ee84d9752fe90f86
RashmiSunkara <91406527+RashmiSunkara@users.noreply.github.com>
2021-10-26 21:21:02 +0530

    Add files via upload

---
+++ intro.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<ctype.h>
+void main(){
+char i;
+int sum = 0,n=0,d=0;
+FILE *file;
+float avg,c=1.0;
+file = fopen("sample.txt", "r+");
+while ((i=(fgetc(file))) != EOF)
+{ 
+    if(isdigit(i))
+    { 
+       n = i - '0';
+       d=d*10+n;
+       printf("%d\n",d);
+    }
+    else
+    {
+        if(i=='\n')
+        {
+            sum=sum+d;
+            d=0;
+            c++;
+        }
+    }
+}
+printf("%d\n",sum+d);
+printf("%f\n",c);
+avg=(sum+d)/c;
+printf("%f",avg);
+fclose(file);
+}
+
+
+
+
+
